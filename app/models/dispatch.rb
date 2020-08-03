145ccaff792a6a2c85dd23ddc0e69a91bbd0c6ad
Kyeongsang Na <na_kyeongsang@mybridge.com>
2020-08-03 15:21:06 +0900

    update: dispatch 모델 구현

--- app/models/dispatch.rb
+++ app/models/dispatch.rb
@@ -1,2 +1,7 @@
 class Dispatch < ApplicationRecord
+
+  validates :address, presence: true, length: { maximum: 100 }
+  validates :passenger_id, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 99999999999 }, allow_nil: false
+  validates :driver_id, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 99999999999 }, allow_nil: true
+  validates :requested_at, presence: true
 end
