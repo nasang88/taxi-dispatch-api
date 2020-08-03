170cf1d5bb71f512a7777926f05ab0f2649893e0
Kyeongsang Na <na_kyeongsang@mybridge.com>
2020-08-03 15:00:45 +0900

    add: dispatch 모델 테스트 코드 작성

--- spec/models/dispatch_spec.rb
+++ spec/models/dispatch_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Dispatch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:address) }
+  it { should validate_length_of(:address).is_at_most(100) }
+  it { should validate_presence_of(:passenger_id) }
+  it { should validate_numericality_of(:passenger_id).only_integer.is_less_than_or_equal_to(99999999999) }
+  it { should validate_numericality_of(:driver_id).only_integer.is_less_than_or_equal_to(99999999999).allow_nil }
+  it { should validate_presence_of(:requested_at) }
 end
