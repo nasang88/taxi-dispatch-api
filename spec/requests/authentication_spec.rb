5c1c0e963c8d3a405350650c098efa9068f8c654
Kyeongsang Na <na_kyeongsang@mybridge.com>
2020-08-04 14:59:22 +0900

    add: request/authentication #로그인 실패 테스트 케이스

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -19,11 +19,19 @@ RSpec.describe  'Authentication', type: :request do
       }.to_json
     end
 
-    context '유효한 로그인' do
+    context '로그인 성공' do
       before { post '/auth/login', params: valid_credentials, headers: headers }
 
       it 'status: 200' do
         expect(response).to have_http_status(200)
+      end
+    end
+
+    context '로그인 실패' do
+      before { post '/auth/login', params: invalid_credentials, headers: headers }
+
+      it 'status: 401' do
+        expect(response).to have_http_status(401)
       end
     end
   end
