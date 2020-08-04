c3c7386104ca5e13cc2ee283360feab428ef1228
Kyeongsang Na <na_kyeongsang@mybridge.com>
2020-08-04 17:59:07 +0900

    update: 리뷰대응, 최상 위 subject 하나만 선언하도록 로직 변경

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 RSpec.describe AuthenticateUser do
   let(:user) { create(:user) }
-  subject(:valid_auth_obj) { described_class.new(user.email, user.password) }
-  subject(:invalid_auth_obj) { described_class.new('foo', 'bar') }
+  let(:email) { user.email }
+  let(:password) { user.password }
+  subject {described_class.new(email, password)}
 
   describe '#call' do
     context '유효한 인증인 경우' do
       it '토큰 반환' do
-        token = valid_auth_obj.call
+        token = subject.call
         expect(token).not_to be_nil
       end
     end
 
     context '무효한 인증인 경우' do
+      let(:email) { 'foobar' }
+
       it 'raises an authentication error' do
-        expect { invalid_auth_obj.call }
+        expect { subject.call }
             .to raise_error( ExceptionHandler::AuthenticationError )
       end
     end
